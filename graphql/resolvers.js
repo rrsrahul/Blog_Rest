493ee8015d27fca2f3692259a59e6e628b51dfb7
Rahul R S <rahulrsgoku@gmail.com>
2020-07-20 01:06:55 +0530

    Adding Api for creating post

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,10 @@
 const bcrypt = require('bcryptjs');
-const User = require('../models/user'); 
+
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user'); 
+const Post = require('../models/post');
 
 module.exports = {
     createUser: async function({ userInput },req){
@@ -43,7 +47,70 @@ module.exports = {
 
         return { ...createdUser._doc,_id:createdUser._id.toString()};
         
+    },
+    login: async function({ email,password },req)
+    {
+        const user = await User.findOne({email:email});
 
+        if(!user)
+        {
+            const err = new Error('No user with the given email');
+            err.status = 401;
+            throw err;
+        }
+        console.log(user.name);
+        const isEqual = await bcrypt.compare(password,user.password);
 
+        if(!isEqual)
+        {
+            const err = new Error('Passwords Do not match');
+            err.status = 403;
+            throw err;
+        }
+
+        const token =  jwt.sign({
+            userId:user._id.toString(),
+            email: user.email
+        },'supersecretsecret',
+        {
+            expiresIn:'1h'
+        });
+
+        return {token:token,userId:(await user)._id.toString()};
+
+    },
+    createPost : async function({ postInput },req)
+    {
+        const errors = [];
+
+        if(validator.isEmpty(postInput.title) || !validator.isLength(postInput.title,{ min:3}))
+        {
+            errors.push({message:'Title is too short'});
+        }
+
+        if(validator.isEmpty(postInput.content) || !validator.isLength(postInput.content,{ min:3}))
+        {
+            errors.push({message:'Content is too short'});
+        }
+
+        if(errors.length>0)
+        {
+            const err = new Error('Invalid Input');
+            err.data = errors;
+            err.code = 422;
+            throw err;
+        }
+
+        const post = new Post({
+            title:postInput.title,
+            content:postInput.content,
+            imageUrl:postInput.imageUrl
+        });
+
+        const createdPost = await post.save();
+
+        return {...createdPost._doc, _id:createdPost._id.toString(),
+            createdAt: createdPost.createdAt.toString(),updatedAt:createdPost.updatedAt.toISOString()
+        };
     }
 }
